cccc5728dfe0e8710a3e9eda44f07c0b90a5aa6a
Michael Schmidt <michael.schmidt@opitz-consulting.com>
2022-12-31 14:04:33 +0100

    add picocli

--- src/main/java/com/github/mschmiudt42/cli/App.java
+++ src/main/java/com/github/mschmiudt42/cli/App.java
@@ -1,13 +1,26 @@
 package com.github.mschmiudt42.cli;
 
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
 /**
  * Hello world!
  *
  */
-public class App 
+@Command(name = "clidemo", mixinStandardHelpOptions = true, version = "clidemo 0.1",
+	description = "just a demo.")
+public class App  implements Callable<Integer> 
 {
     public static void main( String[] args )
     {
+    	int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    public Integer call() throws Exception {
         System.out.println( "Hello World!" );
+        return 0;
     }
 }
