e494d35df07b6fabae5ef452443d9570b5c9f1db
Michael Schmidt <michael.schmidt@opitz-consulting.com>
2022-12-31 12:45:10 +0100

    as generated by maven

---
+++ src/main/java/com/github/mschmiudt42/cli/App.java
@@ -0,0 +1,13 @@
+package com.github.mschmiudt42.cli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
