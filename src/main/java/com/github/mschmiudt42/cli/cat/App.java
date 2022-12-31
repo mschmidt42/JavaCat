a22faed451751d43daf18339e495850da8953c80
Michael Schmidt <michael.schmidt@opitz-consulting.com>
2022-12-31 17:35:08 +0100

    cat <file> is workling

--- src/main/java/com/github/mschmiudt42/cli/cat/App.java
+++ src/main/java/com/github/mschmiudt42/cli/cat/App.java
@@ -1,26 +1,63 @@
 package com.github.mschmiudt42.cli.cat;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.io.IOException;
+
 import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
+import picocli.CommandLine.*;
 
-/**
- * Hello world!
- *
- */
 @Command(name = "cat", mixinStandardHelpOptions = true, version = "cat 0.1",
 	description = "cat")
 public class App  implements Callable<Integer> 
 {
+    @Parameters(arity = "0..*", description = "input file(s)")
+    String[] fileNames;
+
+    @Option( names = {"-d", "--debug"}, description="print debug messages")
+    boolean debug = false;
+
+
     public static void main( String[] args )
     {
     	int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
 
+    private void catFile(String fname){
+        try {
+            Stream<String> stream = Files.lines(Paths.get(fname));
+            catFile(stream);
+        } catch (IOException e) {
+            if(debug) printErr("ERROR: " + e.getClass().getName() + " Msg: " + e.getMessage());
+        }
+
+    }
+
+    private void catFile(Stream<String> stream){
+        stream.forEach(l -> println(l));
+    }
+
+    public void println(String line){
+        System.out.println(line);
+    }
+
+    public void printErr(String line){
+        System.err.println(line);
+    }
+
     public Integer call() throws Exception {
         System.out.println( "Hello World!" );
+
+        if(fileNames != null) {
+            for (String filename : fileNames) {
+                catFile(filename);
+            }
+        }
+
         return 0;
     }
 }
